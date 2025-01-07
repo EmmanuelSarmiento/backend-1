5c933b74507cf793b843e3d1ffcf07072a5f704b
Emmanuel Sarmiento <emmatombino21@gmail.com>
2025-01-07 20:10:49 -0300

    agrego vista de productos

--- src/routes/products.routes.js
+++ src/routes/products.routes.js
@@ -9,88 +9,120 @@ const productRouter = Router();
 const productsPath = path.resolve(__dirname, "../src/db/products.json");
 
 //Leo el archivo json
-const productsData = await fs.readFile(productsPath, "utf-8");
-const products = JSON.parse(productsData);
+async function getProducts() {
+  const productsData = await fs.readFile(productsPath, "utf-8");
+  return JSON.parse(productsData);
+}
 
 //Consulto por un productos
-productRouter.get("/", (req, res) => {
-  const { limit } = req.query;
-  const products = products.splice(0, limit);
-  res.status(200).send(products);
+productRouter.get("/", async (req, res) => {
+  try {
+    const { limit } = req.query;
+    let products = await getProducts();
+    if (limit) {
+      products = products.slice(0, limit);
+    }
+    res.status(200).render("templates/home", { productos: products });
+  } catch (err) {
+    res.status(500).send("Error al leer el archivo de productos");
+  }
 });
 
 //Consulto por un producto segun su id
-productRouter.get("/:pid", (req, res) => {
-  const idProduct = req.params.pid;
-  const product = products.find((prod) => prod.id == idProduct);
-  if (product) {
-    res.status(200).send(product);
-  } else {
-    res.status(404).send({ mensaje: "El producto no existe" });
+productRouter.get("/:pid", async (req, res) => {
+  try {
+    const idProduct = req.params.pid;
+    const products = await getProducts();
+    const product = products.find((prod) => prod.id == idProduct);
+    if (product) {
+      res.status(200).send(product);
+    } else {
+      res.status(404).send({ mensaje: "El producto no existe" });
+    }
+  } catch (err) {
+    res.status(500).send("Error al leer el archivo de productos");
   }
 });
 
 //Creo un nuevo producto
 productRouter.post("/", async (req, res) => {
-  const { title, description, code, price, stock, category } = req.body;
-  const newproducts = {
-    id: crypto.randomBytes(10).toString("hex"),
-    title: title,
-    description: description,
-    code: code,
-    price: price,
-    status: true,
-    stock: stock,
-    category: category,
-    thumbnails: [],
-  };
-  products.push(newproducts);
-  await fs.writeFile(productsPath, JSON.stringify(products)); //Reescribo el nuevo JSON con el nuevo producto
-  res.status(201).send({
-    mensaje: `Producto creado correctamente con el id: ${newproducts.id}`,
-  });
+  try {
+    const { title, description, code, price, stock, category } = req.body;
+    const products = await getProducts();
+    const newProduct = {
+      id: crypto.randomBytes(10).toString("hex"),
+      title,
+      description,
+      code,
+      price,
+      status: true,
+      stock,
+      category,
+      thumbnails: [],
+    };
+    products.push(newProduct);
+    await fs.writeFile(productsPath, JSON.stringify(products));
+    res.status(201).send({
+      mensaje: `Producto creado correctamente con el id: ${newProduct.id}`,
+    });
+  } catch (err) {
+    res.status(500).send("Error al crear el producto");
+  }
 });
 
 //Actualizo un producto dado su id y pido los datos a actualizar del cuerpo de la peticion
 productRouter.put("/:pid", async (req, res) => {
-  const idProduct = req.params.pid;
-  const {
-    title,
-    description,
-    code,
-    price,
-    stock,
-    category,
-    thumbnails,
-    status,
-  } = req.body;
-  const index = products.findIndex((prod) => prod.id == idProduct);
-  if (index != -1) {
-    products[index].title = title;
-    products[index].description = description;
-    products[index].code = code;
-    products[index].price = price;
-    products[index].status = status;
-    products[index].stock = stock;
-    products[index].category = category;
-    products[index].thumbnails = thumbnails;
-    await fs.writeFile(productsPath, JSON.stringify(products));
-    res.status(200).send({ mensaje: "Producto actualizado" });
-  } else {
-    res.status(404).send({ mensaje: "El producto no existe" });
+  try {
+    const idProduct = req.params.pid;
+    const {
+      title,
+      description,
+      code,
+      price,
+      stock,
+      category,
+      thumbnails,
+      status,
+    } = req.body;
+    const products = await getProducts();
+    const index = products.findIndex((prod) => prod.id == idProduct);
+    if (index != -1) {
+      products[index] = {
+        ...products[index],
+        title,
+        description,
+        code,
+        price,
+        stock,
+        category,
+        thumbnails,
+        status,
+      };
+      await fs.writeFile(productsPath, JSON.stringify(products));
+      res.status(200).send({ mensaje: "Producto actualizado" });
+    } else {
+      res.status(404).send({ mensaje: "El producto no existe" });
+    }
+  } catch (err) {
+    res.status(500).send("Error al actualizar el producto");
   }
 });
 
 //Elimino el producto por su id
 productRouter.delete("/:pid", async (req, res) => {
-  const idProduct = req.params.pid;
-  const index = products.findIndex((prod) => prod.id == idProduct);
-  if (index != -1) {
-    products.splice(index, 1);
-    await fs.writeFile(productsPath, JSON.stringify(products));
-    res.status(200).send({ mensaje: "Producto eliminado" });
-  } else {
-    res.status(404).send({ mensaje: "El producto no existe" });
+  try {
+    const idProduct = req.params.pid;
+    const products = await getProducts();
+    const index = products.findIndex((prod) => prod.id == idProduct);
+    if (index != -1) {
+      products.splice(index, 1);
+      await fs.writeFile(productsPath, JSON.stringify(products));
+      res.status(200).send({ mensaje: "Producto eliminado" });
+    } else {
+      res.status(404).send({ mensaje: "El producto no existe" });
+    }
+  } catch (err) {
+    res.status(500).send("Error al eliminar el producto");
   }
 });
 
--- src/server.js
+++ src/server.js
@@ -23,25 +23,24 @@ app.use(express.urlencoded({ extended: true }));
 app.engine("handlebars", hbs.engine);
 app.set("view engine", "handlebars");
 
-app.use("/static", express.static(__dirname + "/public"));
 app.set("views", path.join(__dirname, "views"));
 
+app.use("/public", express.static(__dirname + "/public"));
 app.use("/api/products", productRouter);
 app.use("/api/carts", cartRouter);
 app.use("/upload", multeRouter);
 
-app.get("/", (req, res) => {
-  fs.readFile(
-    path.join(__dirname, "db", "products.json"),
-    "utf-8",
-    (err, data) => {
-      if (err) {
-        return res.status(500).send("Error al leer el archivo de productos");
-      }
-      const products = JSON.parse(data);
-      res.render("products", { products });
-    }
-  );
+app.get("/", async (req, res) => {
+  try {
+    const data = await fs.readFile(
+      path.join(__dirname, "db", "products.json"),
+      "utf-8"
+    );
+    const products = JSON.parse(data);
+    res.render("templates/home", { productos: products });
+  } catch (err) {
+    res.status(500).send("Error al leer el archivo de productos");
+  }
 });
 
 io.on("connection", (socket) => {
